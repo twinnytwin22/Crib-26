28c8165f2ca4c8bf0105f3d77cda3b0d02af5550
Randal Herndon <97874795+twinnytwin22@users.noreply.github.com>
2025-11-12 22:48:15 -0700

    debug seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,9 @@
-import allKeywords from "@/lib/seoKeywords";
+'use client';
 import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { GoogleAnalytics } from '@next/third-parties/google'
 
-export const revalidate = 3600; // Revalidate every hour
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,26 +15,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
-  metadataBase: new URL("https://cribnetwork.io"),
-
-  title: "CRIB",
-  description: "Connect. Revolutionize. Innovate. Boost.",
-
-  generator: "CRIB",
-  applicationName: "CRIB",
-  referrer: "origin-when-cross-origin",
-  keywords: allKeywords,
-  authors: [{ name: "Randal Herndon" }],
-  // colorScheme: "dark",
-  creator: "Randal Herndon",
-  publisher: "Randal Herndon",
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
-};
 
 const gaId:string = process.env.GA_TAG || process.env.NEXT_PUBLIC_GA_TAG ||"";
 
--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,29 @@ import NavBar from "@/components/nav/NavBar";
 import BlogPostsPreview from "@/components/BlogPreview";
 import { getBlogPosts } from "@/lib/providers/sanity/sanity";
 import { Suspense } from "react";
+import allKeywords from "@/lib/seoKeywords";
+
+export const metadata = {
+  metadataBase: new URL("https://cribnetwork.io"),
+
+  title: "CRIB",
+  description: "Connect. Revolutionize. Innovate. Boost.",
+
+  generator: "CRIB",
+  applicationName: "CRIB",
+  referrer: "origin-when-cross-origin",
+  keywords: allKeywords,
+  authors: [{ name: "Randal Herndon" }],
+  // colorScheme: "dark",
+  creator: "Randal Herndon",
+  publisher: "Randal Herndon",
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
+};
+
 
 export default async function Home() {
   const blogPostsPromise = getBlogPosts();
