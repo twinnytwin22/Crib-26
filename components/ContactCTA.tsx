99c545eba185ef3c21a9259c759b8bb50ac24838
Randal Herndon <97874795+twinnytwin22@users.noreply.github.com>
2025-11-16 23:50:40 -0700

    Update ContactCTA.tsx

--- components/ContactCTA.tsx
+++ components/ContactCTA.tsx
@@ -110,7 +110,7 @@ export default function ContactCTA() {
             <div className="space-y-4 text-white/70">
               <div className="flex items-center gap-3">
                 <Mail className="h-5 w-5" />
-                <span>hello@cribdigital.io</span>
+                <span>hello@cribnetwork.io</span>
               </div>
               <div className="flex items-center gap-3">
                 <MapPin className="h-5 w-5" />
